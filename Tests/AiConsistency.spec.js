8c327289592bc08736948b256cea5af352350891
Morad37ELKHAILY <morad73elkhaily@gmail.com>
2025-03-03 23:12:43 +0100

    Add some validation

--- Tests/AiConsistency.spec.js
+++ Tests/AiConsistency.spec.js
@@ -15,7 +15,19 @@ test('Validate AI output consistency', async ({ request }) => {
     await aiService.checkConsistency(responses);
 });
 test('Validate AI logging', async ({ request }) => {
+    const userInput = "Préparer un audit ISO";
+    const rating = 5;
     const aiService = new AIServiceAPI(request);
     // Étape 3 : Soumettre une évaluation et vérifier que le feedback est bien enregistré
-    await aiService.submitRating(5);
+    const response = await aiService.submitRating(userInput,rating);
+    expect(response.status()).toBe(200); // Vérifier que la requête a réussi
+    // Étape 3 : Vérifier que la note a bien été enregistrée en récupérant les logs
+    const logsResponse = await request.get(SaaS_RATING_API_URL, {
+        params: { input: userInput }
+    })
+    expect(logsResponse.status()).toBe(200); // Vérifier que l'API de logs répond
+        const logs = await logsResponse.json();
+        
+    // Étape 4 : Vérifier que le feedback soumis est bien enregistré dans les logs
+    expect(logs).toContainEqual({ input: userInput, rating: rating });
 });
